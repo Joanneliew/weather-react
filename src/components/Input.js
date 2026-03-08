f333ca18afb31830439d8181f9aa45e27c8046f3
Joanne <joanneliew.designer@gmail.com>
2026-03-08 19:11:42 +0800

    test

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,19 @@
-function Input({name, value, onChange, placeholder, label}) {
-    return (
-      <div className={"form-floating"}>
-        <input 
-          type="text" 
-          className={'form-control'}
-          name={name} 
-          value={value} 
-          onChange={onChange} 
-          placeholder={placeholder}
-          required
-        ></input>
-        <label className={`form-label`}>{label}</label>
+function Input({ name, value, onChange, placeholder, label }) {
+  return (
+    <div className={"form-floating"}>
+      <input
+        type="text"
+        className={"form-control"}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        required
+      ></input>
+      <label className={`form-label`}>{label}</label>
+      <label className={`form-label`}>{label}</label>
     </div>
-    );
+  );
 }
 
 export default Input;
